38a6358e855014819937ec35e1901824ebbaaa64
ngxson4160 <ng.xson194160.gmail.com>
2023-05-18 00:34:12 +0700

    check ISP

--- src/main/java/common/interfaces/Observable.java
+++ src/main/java/common/interfaces/Observable.java
@@ -3,6 +3,9 @@ package common.interfaces;
 /**
  * @author
  */
+
+// Interface Observable có 3 phương thức attach(Observer observer), remove(Observer observer),  notifyObservers() đều được sử dụng trong views > home > Mediahandler
+// => KHÔNG vi phạm ISP
 public interface Observable {
 
     void attach(Observer observer);
