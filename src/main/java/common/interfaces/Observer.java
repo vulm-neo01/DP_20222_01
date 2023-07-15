631e09d380b592d0500be74cc96962cd9ac27631
vulm-neo01 <minhvu8a@gmail.com>
2023-07-15 17:55:15 +0700

    Review SOLID LSP - VULM

--- src/main/java/common/interfaces/Observer.java
+++ src/main/java/common/interfaces/Observer.java
@@ -4,8 +4,6 @@ package common.interfaces;
  * @author
  */
 
-//Phương thức update(Observable observable) trong interface Observer được sử dụng trong view > screen > home. Hơn nữa nó chỉ có 1 method
-//=> KHÔNG bi phạm ISP
 public interface Observer {
 
     void update(Observable observable);
